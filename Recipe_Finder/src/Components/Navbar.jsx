65dc50aa04e4aadc6fb27a3b837b1ebccc5b864a
Ansu Winii <khadgiansu@gmail.com>
2026-09-26 20:49:05 +0545

    feat: setup React and Navbar

--- Recipe_Finder/src/Components/Navbar.jsx
+++ Recipe_Finder/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function Navbar({ favoritesCount }) {
   return (
     <header className="navbar">
-      <NavLink to="/" className="navbar__logo">Recipe Finder</NavLink>
+      <NavLink to="/" className="navbar__logo">Recipe_Finder</NavLink>
       <nav className="navbar__links">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/favorites">Favorites ({favoritesCount})</NavLink>
